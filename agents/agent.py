b6f85c1aef2df4538565359c6c0b75771011a83d
russellocean <russellwelch17@gmail.com>
2023-05-20 14:29:53 -0400

    Get agents name and input data in prompt

--- agents/agent.py
+++ agents/agent.py
@@ -89,7 +89,6 @@ class Agent:
                 response = openai.ChatCompletion.create(
                     model="gpt-4",
                     temperature=0.7,
-                    max_tokens=3000,
                     messages=[{"role": "user", "content": prompt}],
                 )
 
